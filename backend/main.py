c5248c16d59cc110999ffdfc3eb987832aa5f968
enriqueiiianonat-ghub <enriqueiiianonat@gmail.com>
2026-05-27 18:11:06 +0800

    uodate fukw
    uodate fukw

--- backend/main.py
+++ backend/main.py
@@ -64,12 +64,15 @@ db_fs = firestore.client()
 # IMPORTANT: Remember to replace MAIL_PASSWORD with a generated 16-character Google App Password
 mail_config = ConnectionConfig(
     MAIL_USERNAME="king@devgloyd.com",
-    MAIL_PASSWORD="wssw nxsa bamf fpqg", 
+    MAIL_PASSWORD="wssw nxsa bamf fpqg",
     MAIL_FROM="king@devgloyd.com",
-    MAIL_PORT=587,
+
     MAIL_SERVER="smtp.gmail.com",
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
+    MAIL_PORT=465,
+
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True
 )
