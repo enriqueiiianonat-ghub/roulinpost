7cffb9c70d1cc8ff1fa418c71077e2dbb5fece3e
enriqueiiianonat-ghub <enriqueiiianonat@gmail.com>
2026-06-10 00:00:32 +0800

    update files
    update files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3684250190" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "694189133" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
